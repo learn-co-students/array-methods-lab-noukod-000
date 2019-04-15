91854dc3e0ba01bdec641025066893aed5eae038
James Declerus <james.declerus@esih.edu>
2019-04-15 15:12:37 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,23 @@
 def using_include(array, element)
-array.include?(element)
+  array.include?(element)
 end
 
 def using_sort(array)
-array.sort
+return array.sort
 end
 
 def using_reverse(array)
-array.Reverse
+return array.reverse
 end
 
 def using_first(array)
-array.first
+return array.first
 end
 
 def using_last(array)
-array.last
+return array.last
 end
 
 def using_size(array)
-
+return array.size
 end
